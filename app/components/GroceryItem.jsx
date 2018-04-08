b6e86176ad391e0b44cd0d996002679769388fbb
snayrouz <samuel.nayrouz@gmail.com>
2018-04-08 12:23:31 -0600

    Add functions to toggle buy or unbuy list item and set to purchased or not purchased

--- app/components/GroceryItem.jsx
+++ app/components/GroceryItem.jsx
@@ -3,16 +3,31 @@ var createReactClass = require("create-react-class");
 var action = require('./../actions/GroceryItemActionCreator.jsx')
 
 module.exports = createReactClass({
+  togglePurchased:function(e){
+    e.preventDefault();
+
+    if (this.props.item.purchased){
+      action.unbuy(this.props.item);
+    } else {
+      action.buy(this.props.item);
+    }
+  },
+
   delete:function(e){
     e.preventDefault();
     action.delete(this.props.item);
   },
+
   render:function(){
     return(
       <div>
         <div>
           <h4 className={this.props.item.purchased ? "strikethrough" : ""}>{this.props.item.name}</h4>
         </div>
+        <form className="three columns" onSubmit={this.togglePurchased}>
+          <button className={this.props.item.purchased ? "" : "button-primary"}>
+            {this.props.item.purchased ? "Unbuy" : "Buy"}</button>
+        </form>
         <form className="three columns" onSubmit={this.delete}>
           <button>&times;</button>
         </form>
--- app/stores/GroceryItemStore.jsx
+++ app/stores/GroceryItemStore.jsx
@@ -36,6 +36,12 @@ function GroceryItemStore(){
     triggerListeners();
   }
 
+  function setGroceryItemBought(item, isBought){
+    var _item = items.filter(function(a){ return a.name == item.name})[0];
+    item.purchased = isBought || false;
+    triggerListeners();
+  }
+
   function onChange(listener){
     listeners.push(listener);
   }
@@ -53,8 +59,17 @@ function GroceryItemStore(){
         case "add":
         addGroceryItem(event.payload);
         break;
+
         case "delete":
         deleteGroceryItem(event.payload);
+        break;
+
+        case "buy":
+        setGroceryItemBought(event.payload, true);
+        break;
+
+        case "unbuy":
+        setGroceryItemBought(event.payload, false);
         break;
       }
     }
