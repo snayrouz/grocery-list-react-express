3e1a4af289c34011108689752303474c9d4f923b
snayrouz <samuel.nayrouz@gmail.com>
2018-04-08 10:48:56 -0600

    Add style and change gulp file

--- app/components/GroceryItem.jsx
+++ app/components/GroceryItem.jsx
@@ -4,7 +4,9 @@ var createReactClass = require("create-react-class");
 module.exports = createReactClass({
   render:function(){
     return(
-      <div>{this.props.item.name}</div>
+      <div>
+        <h4 className={this.props.item.purchased ? "strikethrough" : ""}>{this.props.item.name}</h4>
+      </div>
     )
   }
 })
