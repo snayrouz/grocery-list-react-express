991edb170a7da6e6226323e4389673e750179bfc
snayrouz <samuel.nayrouz@gmail.com>
2018-04-08 11:03:42 -0600

    Add functions for adding item to list with flux

--- app/components/GroceryListAddItem.jsx
+++ app/components/GroceryListAddItem.jsx
@@ -2,11 +2,21 @@ var React = require('react');
 var createReactClass = require("create-react-class");
 
 module.exports = createReactClass({
+  getInitialState:function(){
+    return {input:""};
+  },
+  handleInputName:function(e){
+    this.setState({input : e.target.value})
+  },
+  addItem:function(e){
+    e.preventDefault();
+    console.log("Adding Item", this.state.input);
+  },
   render:function(){
     return (
       <div className='grocery-addItem'>
-        <form>
-          <input />
+        <form onSubmit={this.addItem}>
+          <input value={this.state.input} onChange={this.handleInputName}/>
           <button> Add Item </button>
         </form>
       </div>
